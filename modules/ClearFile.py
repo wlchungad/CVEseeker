3c2ff66f13ce906ad558a768774f979ba1612c00
Chung Wai Lok <125763609+wlchungad@users.noreply.github.com>
2023-12-07 12:24:59 +0800

    Update ClearFile.py
    Added instructions

--- modules/ClearFile.py
+++ modules/ClearFile.py
@@ -1,12 +1,17 @@
 import os
 import shutil
 from datetime import datetime
+from . import setting
 
 def ClearTemp():
     fileList = ["CVE List.txt", "output.csv", "Title.txt"]
     fileOut = str("Processed_Patch_List_{}.xlsx".format(datetime.now().date()))
-    fileList.append(fileOut)
+    if setting.alertType == "MS": # special case: MSRC (only delete if MS is triggered)
+        fileList.append(fileOut)
+    else:
+        print("Note: {} is not downloaded.".format(fileOut))
     folderPath = './output/' + str(datetime.now().date())
+    # move and delete
     if not os.path.exists(folderPath):
         os.makedirs(folderPath)
     for item in fileList:
@@ -16,6 +21,8 @@ def ClearTemp():
           # print("{} moved to {}.".format(item, folderPath))
         else:
           print(" - Exception: {} does not exist.".format(item))
+
+    # remove temp dir
     current_directory = os.getcwd()
     tempDir = os.path.join(current_directory, r'temp')
     if os.path.exists(tempDir):
