6070d60e30000584f3e3bb95d52c593c35b36290
John Doe <chungwailok@gmail.com>
2023-12-13 14:52:09 +0800

    Bug fixed

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from datetime import datetime
 import fnmatch, os, re
 from urllib.parse import urlparse, urlsplit
 
+
 def getLink(hintMsg):
     while True:
         try:
@@ -20,10 +21,10 @@ def getLink(hintMsg):
         except ValueError:
             print("Sorry, I don't understand that.")
             continue
-        if not re.search("www.govcert.gov.hk", value): # not following the format
-            print ("The link does not follows the format.")
-            print ("Example link:") 
-            print ("https://www.govcert.gov.hk/en/alerts_detail.php?id=1154")
+        if not re.search("www.govcert.gov.hk", value):  # not following the format
+            print("The link does not follows the format.")
+            print("Example link:")
+            print("https://www.govcert.gov.hk/en/alerts_detail.php?id=1154")
             continue
         else:
             parsed_url = urlparse(value)
@@ -35,6 +36,7 @@ def getLink(hintMsg):
                 continue
     return value
 
+
 def main():
     # Step 0: Initialize global variable
     setting.init()
@@ -43,7 +45,7 @@ def main():
     while exited == False:
         while True:
             answer = input("Do you need the service? [Y/N]")
-            if answer in allowedNo:                
+            if answer in allowedNo:
                 # Sweep files to output folder
                 ClearFile.ClearTemp()
                 print("Files are moved and deleted.")
@@ -54,13 +56,16 @@ def main():
             elif answer in allowedYes or answer == "":
                 # Step 1: Get the list of CVE and expand for Excel (i.e. CVE-2023-xxxx to xxxx)
                 print("----------------------------------------")
+                setting.alertType == ""
                 LinkProcessor.process_GOV_Link(getLink("Please enter the link: "))
                 print("Getting CVEs from GovCERT...")
                 if setting.alertType == "MS":
                     print("Downloading MSRC...")
                     # Step 2: highlight related items for easier reading
-                    MSRC.downloadFile() # Step 2.1: get raw file
-                    PatchListProcessor.process_patchlist(MSRC.patchName, setting.ProductList)# Step 2.2: process file
+                    MSRC.downloadFile()  # Step 2.1: get raw file
+                    PatchListProcessor.process_patchlist(
+                        MSRC.patchName, setting.ProductList
+                    )  # Step 2.2: process file
                 else:
                     print(" - No need to download MSRC files.")
                     pass
@@ -72,6 +77,8 @@ def main():
             else:
                 print("Invalid input")
                 continue
-    return 
-if __name__=="__main__": 
+    return
+
+
+if __name__ == "__main__":
     main()
