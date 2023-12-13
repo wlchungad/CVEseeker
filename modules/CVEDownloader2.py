53aee98d5774396049a952d4f658096e201a09a5
John Doe <chungwailok@gmail.com>
2023-12-13 14:09:25 +0800

    CVE Module 2 Updated

--- modules/CVEDownloader2.py
+++ modules/CVEDownloader2.py
@@ -15,8 +15,7 @@ def download_problems(importList, type = None):
     count = 0
     # For later changes (if cve.org or Microsoft changes again...)
     MSRC_Initial = "https://msrc.microsoft.com/update-guide/vulnerability/"
-    CVE_Initial = "https://www.cve.org/CVERecord?id="
-    #CVEInitial = "https://cve.mitre.org/cgi-bin/cvename.cgi?name="
+    CVE_Initial = "https://www.cve.org/CVERecord?id=" # replaced with newer URL (old one to be phased out from 1/1/2024) 
     with open("CVE List.txt", "r") as txt_file:
         for line in txt_file.readlines():
             count += 1
@@ -28,6 +27,7 @@ def download_problems(importList, type = None):
     with open("Title.txt", "r") as txt_file:
         for line in txt_file.readlines():
             alertInfo.append(line.replace('\n', ''))
+    # some CVE are useless for the system (e.g. Apple/ Android) or unrelated
     if any(item in alertInfo[1] for item in setting.uselessSet) : 
         print(" - not usable for us")
         Code = setting.lastCVE
@@ -38,20 +38,21 @@ def download_problems(importList, type = None):
             MsgUnrelated += str(alertInfo[1].replace("Vulnerability in ",""))
         with open('output.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            # writer.writerow(['Security Alert Number',' ','Related to ERKS system?','CVE-ID', 'Required?', 'Justification'])
+            # first row already filled in setting.py
             writer.writerow([alertInfo[0],alertInfo[1],'No',Code,'No',MsgUnrelated.strip()])
-            #csvfile.close()
         return
+    # let's be conservative and assume all other vulnerabilities are related
     elif temp != []:
         driver = FP.FFdriver()
-        with open('output.csv', 'w+', newline='') as csvfile:
+        # to append each big item and sub-item to csv
+        with open('output.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Security Alert Number',' ','Related to ERKS system?','CVE-ID', 'Required?', 'Justification'])
+            # writer.writerow(['Security Alert Number',' ','Related to ERKS system?','CVE-ID', 'Required?', 'Justification'])
             isFirstRow = True
             for each in temp:
                 driver.get(each)
                 time.sleep(5)
-                if type == "MS":
+                if type == "MS": # Better consult official information first, IF they are available
                     retries = 3
                     time.sleep(10)
                     Context = ""
@@ -64,11 +65,11 @@ def download_problems(importList, type = None):
                             print ("trying again in 5 seconds ...")
                             time.sleep(5)
                             retries -= 1
-                else:
-                    Code = str(driver.find_element(By.XPATH, "//h1[@class='title']").text)
-                    Context = str(driver.find_element(By.XPATH, "//div[@id='cve-desciption']/p").text)
+                else: # normally, we can just ask cve.org
+                    Code = str(driver.find_element(By.XPATH, "//h1[@class='title']").text).strip()
+                    Context = str(driver.find_element(By.XPATH, "//div[@id='cve-desciption']/p").text).strip()
                     if "Chromium security severity" in Context: # Edge specific, else continue
-                        Context = str((Context.split(" in Google Chrome prior to"))[0])
+                        Context = str((Context.split(" in Google Chrome prior to"))[0]).strip()
                 if isFirstRow:
                     writer.writerow([alertInfo[0],alertInfo[1],'Yes',Code,'Yes',Context])
                     isFirstRow = False
