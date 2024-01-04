efd1797ae3da2a963454246f967c55280ebd8f5a
John Doe <chungwailok@gmail.com>
2024-01-04 10:34:04 +0800

    Improved UI for understanding and bugfixes

--- modules/ClearFile.py
+++ modules/ClearFile.py
@@ -6,24 +6,20 @@ from . import setting
 def ClearTemp():
     fileList = ["CVE List.txt", "output.csv", "Title.txt"]
     fileOut = str("Processed_Patch_List_{}.xlsx".format(datetime.now().date()))
-    if setting.alertType == "MS": # special case: MSRC (only delete if MS is triggered)
-        fileList.append(fileOut)
-    else:
-        print("Note: {} is not downloaded.".format(fileOut))
+    fileList.append(fileOut)
     folderPath = './output/' + str(datetime.now().date())
     # move and delete
-    if not os.path.exists(folderPath):
+    if not os.path.exists(folderPath): # create output sotrage folder
         os.makedirs(folderPath)
     for item in fileList:
-        if os.path.exists(item):
+        if os.path.exists(item): # use copy and remove to "move" safely
           shutil.copy2(item, folderPath)
           os.remove(item)
-          # print("{} moved to {}.".format(item, folderPath))
         else:
           print(" - Exception: {} does not exist.".format(item))
 
     # remove temp dir
     current_directory = os.getcwd()
-    tempDir = os.path.join(current_directory, r'temp')
+    tempDir = os.path.join(current_directory, r'temp') # previously set temp folder is in the same folder of execution
     if os.path.exists(tempDir):
         os.rmdir(tempDir)
--- modules/PatchListProcessor.py
+++ modules/PatchListProcessor.py
@@ -1,37 +1,39 @@
-import openpyxl
+#import openpyxl
 from openpyxl.styles import PatternFill
 from openpyxl import Workbook, load_workbook
-import shutil
+#import shutil
 from datetime import datetime
 import os
 # Function specifically for MSRC CVE table - To filter with colors
 def process_patchlist(filename, importList):
     ProductList = importList # get the "must have" list
-    wb = load_workbook(filename) 
-    sheet = wb['Security Updates'] 
+    try:
+        wb = load_workbook(filename) 
+        sheet = wb['Security Updates'] 
 
-    # read the CVE List generated previously
-    CVEList = []
-    with open("CVE List.txt", "r") as txt_file:
-        lines = [line.strip() for line in txt_file]
-        for _ in lines:
-            CVEList.append(_)
-            
-    for row in range(2, sheet.max_row + 1): 
-        Product = sheet.cell(row, 2).value
-        CVE_code = sheet.cell(row, 9).value
-        if (CVE_code in CVEList):
-            if (Product in ProductList) or (("Microsoft .NET Framework" in Product) and (sheet.cell(row, 3).value == "Windows Server 2016")): 
-                # situation 1: The software is in the "must-have" list
-                # situation 2: The software is .NET-related
-                sheet.cell(row, 2).fill = PatternFill("solid", start_color="FFa500")
-            else: # 2nd priority: related to the current batch, but may not be required
-                sheet.cell(row, 2).fill = PatternFill("solid", start_color="c6efce")
-        else: # last priority: unrelated to current batch, not usable/ no need to record
-             sheet.cell(row, 9).fill = PatternFill("solid", start_color="dcdcdc")
-    wb.save(filename)
-    # rename to Processed_Patch_List_YYYY-MM-DD
-    os.rename(filename, str("Processed_Patch_List_{}.xlsx".format(datetime.now().date())))
-    
+        # read the CVE List generated previously
+        CVEList = []
+        with open("CVE List.txt", "r") as txt_file:
+            lines = [line.strip() for line in txt_file]
+            for _ in lines:
+                CVEList.append(_)
+                
+        for row in range(2, sheet.max_row + 1): 
+            Product = sheet.cell(row, 2).value
+            CVE_code = sheet.cell(row, 9).value
+            if (CVE_code in CVEList):
+                if (Product in ProductList) or (("Microsoft .NET Framework" in Product) and (sheet.cell(row, 3).value == "Windows Server 2016")): 
+                    # situation 1: The software is in the "must-have" list
+                    # situation 2: The software is .NET-related
+                    sheet.cell(row, 2).fill = PatternFill("solid", start_color="FFa500")
+                else: # 2nd priority: related to the current batch, but may not be required
+                    sheet.cell(row, 2).fill = PatternFill("solid", start_color="c6efce")
+            else: # last priority: unrelated to current batch, not usable/ no need to record
+                sheet.cell(row, 9).fill = PatternFill("solid", start_color="dcdcdc")
+        wb.save(filename)
+        # rename to Processed_Patch_List_YYYY-MM-DD
+        os.rename(filename, str("Processed_Patch_List_{}.xlsx".format(datetime.now().date())))
+    except FileNotFoundError:
+        print (" - Workbook not found, probably due to not downloading successfully.")
 if __name__=="__main__": 
     print("Patch List Processor Called")
